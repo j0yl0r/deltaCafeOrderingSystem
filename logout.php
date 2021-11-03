35125377532d0c6fd6c1785edeb1dffd18302deb
j0yl0r <59902577+j0yl0r@users.noreply.github.com>
2021-11-02 20:21:47 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,12 @@
 <!-- logout, so the session will end -->
 <?php
-session_start();
+session_start(); 
+
+// if the username and password match the database it will destroy(unset) when the user logout
 if(isset($_SESSION['username']) && isset($_SESSION['password'])){
     unset($_SESSION['username']);
     unset($_SESSION['password']);
 }
+// Once the user logout it will go back to the login page
 header("Location: ./login.php");
 ?>
